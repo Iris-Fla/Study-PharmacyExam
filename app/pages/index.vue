881bea77e5ae3eb4f25ee136d41f827654745e69
Iris-Fla <irith.fla@gmail.com>
2024-07-09 11:58:02 +0900

    0709

---
+++ app/pages/index.vue
@@ -0,0 +1,10 @@
+<script setup>
+const { data } = await useFetch("/api/test", {
+    method: "GET",
+});
+console.log(data);
+</script>
+<template>
+    <h1>Hellao</h1>
+    <p>{{ data }}</p>
+</template>
