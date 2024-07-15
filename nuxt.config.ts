9c3efe9851ea66d31ae608a9d8f9d73a5e523d6e
Meru🔮 <irith.fla@gmail.com>
2024-07-15 19:22:41 +0900

    package update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  future: {
+    compatibilityVersion: 4
+  },
+
   devtools: { enabled: true },
+
   vite: {
     server: {
       watch: {
@@ -8,4 +13,6 @@ export default defineNuxtConfig({
       },
     },
   },
+
+  compatibilityDate: "2024-07-15",
 });
